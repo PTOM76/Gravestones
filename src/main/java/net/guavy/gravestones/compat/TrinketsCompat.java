c50f70f6a17e44cf9d5a1e3576139de56d05ebd8
JordanPlayz158 <JordanAdamsBusiness@outlook.com>
2024-11-05 22:01:57 -0500

    Still needs more testing but might, ACTUALLY fix the issue with trinkets?

--- src/main/java/net/guavy/gravestones/compat/TrinketsCompat.java
+++ src/main/java/net/guavy/gravestones/compat/TrinketsCompat.java
@@ -2,9 +2,11 @@ package net.guavy.gravestones.compat;
 
 import dev.emi.trinkets.api.TrinketInventory;
 import dev.emi.trinkets.api.TrinketsApi;
+import java.util.Collection;
+import java.util.function.Consumer;
 import net.guavy.gravestones.api.GravestonesApi;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.inventory.Inventory;
+import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
 
 import java.util.ArrayList;
@@ -16,29 +18,36 @@ public class TrinketsCompat implements GravestonesApi {
     public List<ItemStack> getInventory(PlayerEntity entity) {
         List<ItemStack> itemStacks = new ArrayList<>();
 
-        Map<String, Map<String, TrinketInventory>> inventories = TrinketsApi.getTrinketComponent(entity).get().getInventory();
-        for (Map<String, TrinketInventory> map : inventories.values()) {
-            for (TrinketInventory inventory : map.values()) {
-                for(int i = 0; i < inventory.size(); i++) {
-                    itemStacks.add(inventory.getStack(i));
-                }
-            }
-        }
+        forEachTrinket(entity, itemStacks::add);
         return itemStacks;
     }
 
     @Override
     public void setInventory(List<ItemStack> stacks, PlayerEntity entity) {
-        for(ItemStack itemStack : stacks) {
-            Map<String, Map<String, TrinketInventory>> inventories = TrinketsApi.getTrinketComponent(entity).get().getInventory();
-            for (Map<String, TrinketInventory> map : inventories.values()) {
-                for (TrinketInventory inventory : map.values()) {
-                    for(int i = 0; i < inventory.size(); i++) {
-                        if (inventory.getStack(i).isEmpty()) {
-                            inventory.setStack(i, itemStack);
+        PlayerInventory playerInventory = entity.getInventory();
+        Collection<Map<String, TrinketInventory>> inventories = getTrinketInventory(entity).values();
+
+        // Yes this is unbelievably awful, I am both sorry and not sorry... schrodinger's apology
+        for (int i = 0; i < stacks.size(); i++) {
+            ItemStack item = stacks.get(i);
+
+            int size = 0;
+
+            for (Map<String, TrinketInventory> inventoryMap : inventories) {
+                for (TrinketInventory inventory : inventoryMap.values()) {
+                    for (int j = 0; j < inventory.size(); j++, size++) {
+                        //GLFW.glfwSetInputMode(MinecraftClient.getInstance().getWindow().getHandle(), GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+
+                        if (i != size) {
                             continue;
                         }
-                        entity.getInventory().insertStack(itemStack);
+
+                        if (inventory.getStack(j).isEmpty()) {
+                            inventory.setStack(j, item);
+                            continue;
+                        }
+
+                        playerInventory.insertStack(item);
 
                     }
                 }
@@ -48,10 +57,24 @@ public class TrinketsCompat implements GravestonesApi {
 
     @Override
     public int getInventorySize(PlayerEntity entity) {
-        return TrinketsApi.getTrinketComponent(entity).get().getInventory().size();
+        return getTrinketInventory(entity).size();
     }
 
     public static void dropAll(PlayerEntity entity) {
-        TrinketsApi.getTrinketComponent(entity).get().getInventory().clear();
+        getTrinketInventory(entity).clear();
+    }
+
+    private static void forEachTrinket(PlayerEntity entity, Consumer<ItemStack> callable) {
+        for (Map<String, TrinketInventory> map : getTrinketInventory(entity).values()) {
+            for (TrinketInventory inventory : map.values()) {
+                for(int i = 0; i < inventory.size(); i++) {
+                    callable.accept(inventory.getStack(i));
+                }
+            }
+        }
+    }
+
+    private static Map<String, Map<String, TrinketInventory>> getTrinketInventory(PlayerEntity entity) {
+        return TrinketsApi.getTrinketComponent(entity).get().getInventory();
     }
 }
